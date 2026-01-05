117f7a822b548ad5f96b4199a49985a22e767c47
Christopher <ckingmaster2@gmail.com>
2026-01-05 02:18:37 +0100

    feat(exception): add FruitNotFoundException

---
+++ src/main/java/cat/itacademy/s04/s02/n01/fruit/exception/FruitNotFoundException.java
@@ -0,0 +1,8 @@
+package cat.itacademy.s04.s02.n01.fruit.exception;
+
+public class FruitNotFoundException extends RuntimeException {
+
+    public FruitNotFoundException(Long id) {
+        super("Fruit with id " + id + " not found");
+    }
+}
