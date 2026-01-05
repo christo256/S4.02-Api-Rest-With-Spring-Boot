88d1f17ed1b0047a3b702797358ee2723fe09a7d
Christopher <ckingmaster2@gmail.com>
2026-01-05 01:56:07 +0100

    feat(model): add Fruit JPA entity

---
+++ src/main/java/cat/itacademy/s04/s02/n01/fruit/model/Fruit.java
@@ -0,0 +1,24 @@
+package cat.itacademy.s04.s02.n01.fruit.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "fruits")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Fruit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int weightInKilos;
+}
